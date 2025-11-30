4cf82e278e2ff3548b0fc2fbe05f55389a021608
Andrew Murgola <andy@murgo.la>
2025-11-30 12:27:17 -0500

    Forgot another file

---
+++ src/WhisperWriter/TrayIconViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WhisperWriter;
+
+/// <summary>
+/// View model for the system tray icon commands.
+/// </summary>
+public class TrayIconViewModel
+{
+    public ICommand ShowWindowCommand { get; }
+    public ICommand OpenSettingsCommand { get; }
+    public ICommand ExitCommand { get; }
+
+    public TrayIconViewModel(
+        Action showWindowAction,
+        Action openSettingsAction,
+        Action exitAction)
+    {
+        ShowWindowCommand = new RelayCommand(showWindowAction);
+        OpenSettingsCommand = new RelayCommand(openSettingsAction);
+        ExitCommand = new RelayCommand(exitAction);
+    }
+}
